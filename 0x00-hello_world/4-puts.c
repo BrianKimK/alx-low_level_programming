e228c49e89555bcaa626fd8aa6c3f50819f6eb31
BrianKimK <briankimanthik@gmail.com>
2022-12-08 14:52:10 -0800

    misstake

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-/*
- * main - print a of code line using puts
- * Return: 0 
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle"):
-		return (0):
-}
+#include <stdio.h> 
