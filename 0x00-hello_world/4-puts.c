7bbff9bba79c5233a2349ea5b41749fbc0de5621
BrianKimK <briankimanthik@gmail.com>
2022-12-08 14:39:36 -0800

    c language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**                                                                                                                                     
- *  * main - Entry                                                                                                                         
- *   * Return: Always 0                                                                                                                     
- *    */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
-			        return (0);
+/*
+ * main - print a of code line using puts
+ * Return: 0 
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"):
+		return (0):
 }
