6b368f38b56b2e1ab2a0bfaf36427694ea0cfd68
BrianKimK <briankimanthik@gmail.com>
2022-12-08 13:50:27 -0800

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #!/bin/bash
-
+/**                                                                                                                                     
+ *  * main - Entry                                                                                                                         
+ *   * Return: Always 0                                                                                                                     
+ *    */                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                       
+	        puts("\"Programming is like building a multi lingual puzzle);                                                            
+			        return (0);
+}
