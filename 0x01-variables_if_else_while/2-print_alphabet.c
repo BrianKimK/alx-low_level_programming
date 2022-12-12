27a31c05dc18256f4b667fd993b6f4d5d49c858c
BrianKimK <briankimanthik@gmail.com>
2022-12-12 13:14:16 -0800

    second

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - lowercase printing using code
+ * main - Print in lowercase
  *
  * Return: 0
  */
@@ -9,9 +9,10 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= b'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	putchar('\n');
 
 	return (0);
 }
+
