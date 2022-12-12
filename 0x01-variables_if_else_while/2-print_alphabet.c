9393bfb12e0f5809024ebe555b107b4a199d4ab3
BrianKimK <briankimanthik@gmail.com>
2022-12-12 13:02:55 -0800

    second

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase printing using code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= b'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
